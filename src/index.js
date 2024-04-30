7f0e180fca26005984afa7c23b2f654369ca2bd8
SMT <mohamedtawfeeqprogrammer25@gmail.com>
2024-04-30 10:53:24 +0530

    m1

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const App=()=>{
 	<>
 	<BrowserRouter>
 	<Routes>
-	<Route index path="/*" element={<Home/>}/>
+	<Route index path="*" element={<Home/>}/>
 	<Route  path="/game/dualPlayer" element={<Game/>}/>
 	<Route  path="/game/playervsbot" element={<Botmode/>}/>	
 	</Routes>
